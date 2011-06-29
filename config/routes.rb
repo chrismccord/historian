f1194100a371266c31b048062513dba7b6641b43
Chris McCord <chris@chrismccord.com>
2011-06-28 21:26:45 -0400

    Add main historian route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match "#{Historian.mount_point}/main" => 'historian/categories#index', :as => 'historian'
   scope Historian.mount_point do
     resources :categories, :controller => 'historian/categories', :as => 'historian_categories'
     resources :records, :controller => 'historian/records', :as => 'historian_records'
