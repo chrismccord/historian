b0050e317adb901d36d027066409d28fbe65f4c3
Chris McCord <chris@chrismccord.com>
2011-07-12 14:57:49 -0400

    Add support for creating records with specific created_at times

--- lib/historian.rb
+++ lib/historian.rb
@@ -11,15 +11,18 @@ module Historian
   end
   
   def self.history_for(category_name)
-    HistorianRecord.where("historian_category_id = ?", HistorianCategory.find_by_name(category_name).id).order('created_at DESC') rescue nil   
+    cat_id = HistorianCategory.find_by_name(category_name).id
+    HistorianRecord.where("historian_category_id = ?", cat_id).order('created_at DESC') if cat_id
   end
 
   # Record values for given category name.
   # Autocreates new category with inferred titles from hash keys if category does not exist
   # and Historian.auto_create is true, raises not found error otherwise
   #
-  def self.record(category_name, record_values)
-
+  def self.record(category_name, params = {})
+    record_values = params[:data] || raise(ArgumentError, 'Historian record must include :data')
+    created_at    = params[:at] || Time.now
+    
     category = HistorianCategory.find_by_name(category_name)
     if category.nil? && Historian.auto_create
       category = HistorianCategory.create(:name => category_name.to_s,
@@ -31,7 +34,9 @@ module Historian
     new_record = HistorianRecord.new(:category => category)
     record_values.collect do |key, value|
       new_record.metrics << HistorianMetric.new(:name   => key.to_s,
-                                                :value  => value.to_yaml)
+                                                :value  => value.to_yaml,
+                                                :created_at => created_at)
+      new_record.created_at = created_at
     end
 
     new_record.save
